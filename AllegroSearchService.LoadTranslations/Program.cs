47746a5d8bc931601d9828aa77d8176668c97b67
igorvynokurov <igorvynokurov@gmail.com>
2020-11-04 09:51:55 +0200

    fix prev

---
+++ AllegroSearchService.LoadTranslations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AllegroSearchService.LoadTranslations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
