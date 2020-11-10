64e4c7a23f98f31560f81fed9b51f76fc254fef7
Sergii Popil <popil.developer@outlook.com>
2020-11-10 09:37:39 +0200

    add TestKendo

---
+++ AllegroSearchService.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using AllegroSearchService.Api.ViewModels;
+
+namespace AllegroSearchService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : Controller
+    {
+        //public ActionResult EmployeeList([DataSourceRequest] DataSourceRequest request)
+        //{
+        //    try
+        //    {
+        //        List<Employee> _emp = new List<Employee>();
+        //        _emp.Add(new Employee(1, "Bobb", "Ross"));
+        //        _emp.Add(new Employee(2, "Pradeep", "Raj"));
+        //        _emp.Add(new Employee(3, "Arun", "Kumar"));
+        //        DataSourceResult result = _emp.ToDataSourceResult(request);
+        //        return Json(result, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return Json(ex.Message, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+
+        //    }
+        //}
+    }
+}
---
+++ TestKendo/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TestKendo.Models;
+using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+
+namespace TestKendo.Controllers
+{
+    
+    public class EmployeeController : Controller
+    {
+    public ActionResult EmployeeList([DataSourceRequest] DataSourceRequest request)
+        {
+            try
+            {
+                List<Employee> _emp = new List<Employee>();
+                _emp.Add(new Employee(1, "Igor", "Test1"));
+                _emp.Add(new Employee(2, "Ivan", "Test12"));
+                _emp.Add(new Employee(3, "Andy", "Test123"));
+                DataSourceResult result = _emp.ToDataSourceResult(request);
+                return Json(result, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json(ex.Message, System.Web.Mvc.JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
